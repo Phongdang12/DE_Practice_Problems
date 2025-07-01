84ffac7b8792b8571d27c78960d93ea268c2315b
phongdang12 <phong.danghandsomek@hcmut.edu.vn>
2025-07-01 18:54:30 +0700

    a

--- Exercises/Exercise-3/main.py
+++ Exercises/Exercise-3/main.py
@@ -1,8 +1,28 @@
 import boto3
+from botocore import UNSIGNED
+from botocore.config import Config
 
+# Với Credentials	                 Với UNSIGNED
+# ✅ Truy cập private buckets	❌ Chỉ truy cập public buckets
+# ✅ Đầy đủ permissions	        ✅ Chỉ read public data
+# ❌ Cần setup AWS credentials	✅ Không cần credentials
+# ❌ Phức tạp hơn	            ✅ Đơn giản hơn
+
+#.gz là định dạng nén nhưng khác với .zip
+#gz dùng nén một file đơn lẻ(WinRAR), trong khi .zip có thể nén nhiều file và thư mục.
 
 def main():
-    # your code here
+    # your code 
+    s3=boto3.client(
+        's3',
+        region_name='us-east-1',
+        config=Config(signature_version=UNSIGNED)
+    )
+    response=s3.get_object(Bucket='commoncrawl',Key='crawl-data/CC-MAIN-2022-05/wet.paths.gz')
+    gz_content=response['Body'].read()
+    with open('wet.paths.gz', 'wb') as f:
+        f.write(gz_content)
+    
     pass
 
 
--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,8 +1,39 @@
-import boto3
+import glob
+import json
+import csv
+from flatten_json import flatten
 
-
+def write_to_csv(data, filename):
+    """Write flattened data to CSV file"""
+    if not data:
+        return
+    # Write the results to a `csv` file, one for one with the json file, including the header names.
+    with open(filename,'w',newline='',encoding='utf-8') as csvfile:
+        writer=csv.DictWriter(csvfile,fieldnames=data.keys())
+        writer.writeheader()
+        writer.writerow(data)
 def main():
     # your code here
+    # Crawl the `data` directory with `Python` and identify all the `json` files.
+    src_json=glob.glob("**/*.json",recursive=True)
+    all_json_data=[]
+    for json_file in src_json:
+        with open(json_file,'r') as f:
+            # Load all the `json` files.
+            data=json.load(f)  # Load JSON data as a dictionary
+            # Flatten out the `json` data structure.
+            flattened = flatten(data)
+            all_json_data.append(flattened)
+            csv_file=json_file.replace('.json','.csv')
+            write_to_csv(flattened, csv_file)
+    if all(all_json_data[0].keys()==e.keys() for e in all_json_data):
+        with open("output_all_json.csv","w",newline='',encoding='utf-8') as csvfile:
+            writer=csv.DictWriter(csvfile,fieldnames=all_json_data[0].keys())
+            writer.writeheader()
+            for data in all_json_data:
+                writer.writerow(data)
+    else:
+        print("JSON files have different keys, cannot write to a single CSV file.")
     pass
 
 
